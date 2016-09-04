4df6cd86c57003ecd0eb6fda16a95365c323febd
MATSUMOTO, Katsuyoshi <github@katsyoshi.org>
2016-09-04 21:00:29 +0900

    first

---
+++ lib/anzen/na/password/kanri.rb
@@ -0,0 +1,11 @@
+require "anzen/na/password/kanri/version"
+
+module Anzen
+  module Na
+    module Password
+      module Kanri
+        # Your code goes here...
+      end
+    end
+  end
+end
---
+++ lib/anzen/na/password/kanri/version.rb
@@ -0,0 +1,9 @@
+module Anzen
+  module Na
+    module Password
+      module Kanri
+        VERSION = "0.1.0"
+      end
+    end
+  end
+end
