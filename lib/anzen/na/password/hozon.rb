7893ed6afbabc7b5d44c5b79636060c261d287fd
MATSUMOTO, Katsuyoshi <github@katsyoshi.org>
2016-10-17 00:12:31 +0900

    update password

--- lib/anzen/na/password/hozon.rb
+++ lib/anzen/na/password/hozon.rb
@@ -10,9 +10,34 @@ module Anzen
         def save(service:, account:, password: nil, num: 10, type: :full, file: "~/.anzen-na-password")
           f = File.expand_path(file)
           password ||= Generator.generate(num: num, type: type)
-          CSV.open(f, 'w', 0600){|csv| csv << ["service", "account", "password", "created_at", "updated_at"] } unless File.exists?(f)
+          CSV.open(f, 'w', 0600){|csv| csv << header } unless File.exists?(f)
           CSV.open(f, 'a', 0600){|csv| csv << [service, account, password, Time.now, Time.now] }
           {service: service, account: account, password: password}
+        end
+
+        def update(service:, account:, password: nil, num: 10, type: :full, file: "~/.anzen-na-password")
+          f = File.expand_path(file)
+          ret = nil
+          c = nil
+          CSV.open(f, 'r', 0600) do |csv|
+            c = csv.map do |row|
+              if row[0] == service && row[1] == account
+                row[2] = password || Generator.generate(num: num, type: :full)
+                row[-1] = Time.now
+                ret = row
+              end
+              row
+            end
+          end
+
+          CSV.open(f, 'w', 0600) do |csv|
+            c.each{|row| csv << row }
+          end
+          Hash[c[0].zip(ret)]
+        end
+
+        def header
+          ["service", "account", "password", "created_at", "updated_at"]
         end
       end
     end
