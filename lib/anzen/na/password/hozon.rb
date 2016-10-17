aae8dd4e9b07e75af53e899a1da573300d1635a1
MATSUMOTO, Katsuyoshi <github@katsyoshi.org>
2016-10-17 23:51:07 +0900

    fix deprecate method

--- lib/anzen/na/password/hozon.rb
+++ lib/anzen/na/password/hozon.rb
@@ -10,7 +10,7 @@ module Anzen
         def save(service:, account:, password: nil, num: 10, type: :full, file: "~/.anzen-na-password")
           f = File.expand_path(file)
           password ||= Generator.generate(num: num, type: type)
-          CSV.open(f, 'w', 0600){|csv| csv << header } unless File.exists?(f)
+          CSV.open(f, 'w', 0600){|csv| csv << header } unless File.exist?(f)
           CSV.open(f, 'a', 0600){|csv| csv << [service, account, password, Time.now, Time.now] }
           {service: service, account: account, password: password}
         end
