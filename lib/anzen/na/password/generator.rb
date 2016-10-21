81ed4e57513aee1616bc7919bdca6e815cb18441
MATSUMOTO, Katsuyoshi <github@katsyoshi.org>
2016-10-21 21:13:17 +0900

    use memoize

--- lib/anzen/na/password/generator.rb
+++ lib/anzen/na/password/generator.rb
@@ -6,18 +6,22 @@ module Anzen
       module Generator
         module_function
 
-        def generate(num: , type: :full)
+        def generate(num:, type: :full)
           (0..num.to_i).map{ pattern(type: type).sample }.join
         end
 
         def pattern(type: :basic)
-          password_characters = [('a'..'z'), ('A'..'Z'), (0..9)].map(&:to_a).flatten
           case type
+          when :basic
+            @password_characters ||= [("a".."z"), ("A".."Z"), (0..9)].map(&:to_a).flatten
           when :full
-            password_characters += [('!'..'/'), ('['..'`'), ('{'..'~')].map(&:to_a).flatten
+            @password_characters ||= [("a".."z"), ("A".."Z"), (0..9), ("!".."/"), ("[".."`"), ("{".."~")].map(&:to_a).flatten
           else
-            password_characters
+            @password_characters ||= type.split(//) if String === type
+            @password_characters ||= type if Array === type
+            @password_characters ||= type.to_s.split(//) if Integer === type
           end
+          @password_characters
         end
       end
     end
