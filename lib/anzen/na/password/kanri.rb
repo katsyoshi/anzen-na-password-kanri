1bf12a5d64888a1e90802e1f5fcbdb32474f9536
MATSUMOTO, Katsuyoshi <github@katsyoshi.org>
2016-11-08 23:59:01 +0900

    add kanri module

--- lib/anzen/na/password/kanri.rb
+++ lib/anzen/na/password/kanri.rb
@@ -4,6 +4,25 @@ module Anzen
   module Na
     module Password
       module Kanri
+        module_function
+
+        def agent(command, opt)
+          case command.to_sym
+          when :gen
+            opt.delete(:file)
+            puts Anzen::Na::Password::Generator.generate(opt)
+          when :save
+            Anzen::Na::Password::Hozon.save(opt).each{|k, v| puts "#{k}: #{v}" }
+          when :test
+            opt.delete(:file)
+            result = Anzen::Na::Password::Kyodo.sokutei(opt["password"], user_inputs: opt["user_inputs"])
+            puts "password: #{option.password}"
+            puts "score: #{result.score}"
+            puts "crack time: #{result.crack_time_display}"
+          when :update
+            Anzen::Na::Password::Hozon.save(opt).each{|k, v| puts "#{k}: #{v}" }
+          end
+        end
       end
     end
   end
