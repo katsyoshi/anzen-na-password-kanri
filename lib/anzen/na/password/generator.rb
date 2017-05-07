20c12fc630ac6e652f2a603712f66b2e30d2cc19
MATSUMOTO, Katsuyoshi <github@katsyoshi.org>
2017-05-07 23:42:59 +0900

    update use CharSet for generate password

--- lib/anzen/na/password/generator.rb
+++ lib/anzen/na/password/generator.rb
@@ -1,4 +1,4 @@
-require "anzen/na/password/kanri/version"
+require "anzen/na/password/charset"
 
 module Anzen
   module Na
@@ -7,21 +7,8 @@ module Anzen
         module_function
 
         def generate(num:, type: :full)
-          Array.new(num.to_i){ pattern(type: type).sample }.join
-        end
-
-        def pattern(type: :basic)
-          case type
-          when :basic
-            @password_characters ||= [("a".."z"), ("A".."Z"), (0..9)].map(&:to_a).flatten
-          when :full
-            @password_characters ||= [("a".."z"), ("A".."Z"), (0..9), ("!".."/"), ("[".."`"), ("{".."~")].map(&:to_a).flatten
-          else
-            @password_characters ||= type.split(//) if String === type
-            @password_characters ||= type if Array === type
-            @password_characters ||= type.to_s.split(//) if Integer === type
-          end
-          @password_characters
+          charset = CharSet.new.pattern(type: type)
+          Array.new(num.to_i){ charset.sample }.join
         end
       end
     end
